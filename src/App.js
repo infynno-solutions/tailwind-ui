d4f71b71ee06cd50971d0978854c41c27c9e2842
Kaival-Infynno <kaival.infynno@gmail.com>
2022-09-06 14:15:25 +0530

    after resolve conflict

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import CreditCard1 from "./credit-card/sample1/CreditCard1";
 import CreditCard2 from "./credit-card/sample2/CreditCard2";
 import CreditCard3 from "./credit-card/sample3/CreditCard3";
 import Home from "./Home";
-import Sample1 from "./pricing/sample1/Pricing";
-import Sample2 from "./pricing/sample2/Pricing";
+import Pricing1 from "./pricing/sample1/Pricing";
+import Pricing2 from "./pricing/sample2/Pricing";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <Routes>
         <Route path="/credit-card" element={<CreditCard />} />
         <Route path="/" element={<Home />} />
-        <Route path="/pricing/sample1/Pricing" element={<Sample1 />} />
-        <Route path="/pricing/sample2/Pricing" element={<Sample2 />} />
         <Route path="/credit-card/sample1" element={<CreditCard1 />} />
         <Route path="/credit-card/sample2" element={<CreditCard2 />} />
         <Route path="/credit-card/sample3" element={<CreditCard3 />} />
+        <Route path="/pricing/sample1" element={<Pricing1 />} />
+        <Route path="/pricing/sample2" element={<Pricing2 />} />
       </Routes>
     </div>
   );
