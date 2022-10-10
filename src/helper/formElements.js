0cb6f29d6300f26592189f769fd2225516486f08
Kaival-Infynno <kaival.infynno@gmail.com>
2022-10-10 14:13:24 +0530

    fix:conflict3

--- src/helper/formElements.js
+++ src/helper/formElements.js
@@ -19,11 +19,7 @@ export const formElementsData = {
             <button className="bg-blue-600 h-9 rounded flex justify-center items-center px-6
                 text-white font-medium text-xs leading-tight uppercase shadow-md  
                  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0  
-<<<<<<< HEAD
                  active:shadow-lg transition duration-150 ease-in-out hover:bg-blue-800"> 
-=======
-                 active:shadow-lg transition duration-150 ease-in-out"> 
->>>>>>> a81620c (feature/banner and style)
               Button
             </button>
         </div>
@@ -47,7 +43,6 @@ export const formElementsData = {
         code: `
       <div class="flex justify-center gap-3">
         <div class="flex items-center justify-center">
-<<<<<<< HEAD
           <button class="flex h-9 items-center justify-center rounded bg-blue-600 hover:bg-blue-700 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
         </div>
         <div class="flex items-center justify-center">
@@ -66,26 +61,6 @@ export const formElementsData = {
         </div>
         <div class="flex items-center justify-center">
           <button class="success && flex h-9 items-center justify-center hover:bg-white hover:text-black hover:border-2 hover:border-[black] rounded bg-black px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-=======
-          <button class="flex h-9 items-center justify-center rounded bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded bg-green-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-              
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded bg-gray-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-              
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded bg-red-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded bg-yellow-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded bg-black px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
->>>>>>> a81620c (feature/banner and style)
         </div>
       </div> `,
       },
@@ -146,7 +121,6 @@ export const formElementsData = {
         code: `
         <div class="flex justify-center gap-3">
           <div class="flex items-center justify-center">
-<<<<<<< HEAD
              <button class="flex h-9 items-center justify-center rounded-full bg-blue-600 hover:bg-blue-700 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
            </div>
            <div class="flex items-center justify-center">
@@ -165,26 +139,6 @@ export const formElementsData = {
            </div>
            <div class="flex items-center justify-center">
              <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-black px-6 text-xs hover:bg-white hover:text-black hover:border-2 hover:border-[black] font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-=======
-             <button class="flex h-9 items-center justify-center rounded-full bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-           </div>
-           <div class="flex items-center justify-center">
-             <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-green-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-           </div>
-
-           <div class="flex items-center justify-center">
-             <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-gray-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-           </div>
-
-           <div class="flex items-center justify-center">
-             <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-red-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-           </div>
-           <div class="flex items-center justify-center">
-             <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-yellow-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-           </div>
-           <div class="flex items-center justify-center">
-             <button class="success && flex h-9 items-center justify-center rounded-full border-2 bg-black px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
->>>>>>> a81620c (feature/banner and style)
            </div>
           </div>
 
@@ -208,7 +162,6 @@ export const formElementsData = {
         code: `
         <div class="flex justify-center gap-3">
         <div class="flex items-center justify-center">
-<<<<<<< HEAD
           <button class="flex h-9 items-center justify-center rounded-full border-2 border-blue-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-blue-600 hover:bg-gray-100 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
         </div>
         <div class="flex items-center justify-center">
@@ -227,26 +180,6 @@ export const formElementsData = {
         </div>
         <div class="flex items-center justify-center">
           <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-black bg-white px-6 text-xs font-medium uppercase leading-tight text-black shadow-md hover:bg-gray-100 transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-=======
-          <button class="flex h-9 items-center justify-center rounded-full border-2 border-blue-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-blue-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-green-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-green-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-gray-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-gray-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-red-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-red-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-yellow-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-yellow-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        </div>
-        <div class="flex items-center justify-center">
-          <button class="success && flex h-9 items-center justify-center rounded-full border-2 border-black bg-white px-6 text-xs font-medium uppercase leading-tight text-black shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
->>>>>>> a81620c (feature/banner and style)
         </div>
       </div>
         `,
@@ -263,19 +196,11 @@ export const formElementsData = {
           </div>
         ),
         code: `
-<<<<<<< HEAD
         <button class="flex h-7 items-center justify-center rounded bg-blue-600 hover:bg-blue-800 px-5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
 
         <button class="flex h-9 items-center justify-center rounded bg-blue-600 hover:bg-blue-800 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
 
         <button class="flex h-10 items-center justify-center rounded bg-blue-600 hover:bg-blue-800 px-8 text-lg font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-=======
-        <button class="flex h-7 items-center justify-center rounded bg-blue-600 px-5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-
-        <button class="flex h-9 items-center justify-center rounded bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-
-        <button class="flex h-10 items-center justify-center rounded bg-blue-600 px-8 text-lg font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
->>>>>>> a81620c (feature/banner and style)
         `,
       },
       {
@@ -289,13 +214,8 @@ export const formElementsData = {
           </div>
         ),
         code: `
-<<<<<<< HEAD
         <button class="flex h-9 cursor-not-allowed items-center justify-center rounded bg-blue-600 hover:bg-blue-800 px-6 text-xs font-medium uppercase leading-tight text-white opacity-60 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
         <button class="flex h-9 cursor-not-allowed items-center justify-center rounded bg-gray-600 hover:bg-gray-800 px-6 text-xs font-medium uppercase leading-tight text-white opacity-60 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-=======
-        <button class="flex h-9 cursor-not-allowed items-center justify-center rounded bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white opacity-60 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        <button class="flex h-9 cursor-not-allowed items-center justify-center rounded bg-gray-600 px-6 text-xs font-medium uppercase leading-tight text-white opacity-60 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
->>>>>>> a81620c (feature/banner and style)
 
         `,
       },
@@ -310,15 +230,9 @@ export const formElementsData = {
           </div>
         ),
         code: `
-<<<<<<< HEAD
         <button class="mx-5 flex h-9 w-full items-center justify-center rounded bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 hover:bg-blue-800 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
 
         <button class="mx-5 flex h-9 w-full items-center justify-center rounded border-2 border-blue-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-blue-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none hover:bg-gray-100 focus:ring-0 active:shadow-lg">Button</button>
-=======
-        <button class="mx-5 flex h-9 w-full items-center justify-center rounded bg-blue-600 px-6 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-        <button class="mx-5 flex h-9 w-full items-center justify-center rounded border-2 border-blue-600 bg-white px-6 text-xs font-medium uppercase leading-tight text-blue-600 shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">Button</button>
-
->>>>>>> a81620c (feature/banner and style)
         `,
       },
     ],
