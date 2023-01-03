a27107fa64d4ed4d2213e189f9023fd596b0a447
Kaival-Infynno <kaival.infynno@gmail.com>
2023-01-03 14:34:38 +0530

    feature/animation components and buttons

--- src/preview-template/Footer.jsx
+++ src/preview-template/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Logo from "../assets/Logo.svg";
+import Logo from "../assets/Logo.png";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center  items-center p-8 border-t border-gray-200">
+    <div className="flex flex-col justify-center items-center p-8 border-t border-gray-200">
       <a href="/">
-        <img src={Logo} alt="Main logo" className="h-20 w-40 cursor-pointer" />
+        <div className="object-cover w-44 h-20 cursor-pointer">
+          <img src={Logo} alt="Main logo" />
+        </div>
       </a>
       <div className="text-gray-600">
         © {new Date().getFullYear()} Infynno Solutions LLP. All rights reserved.
